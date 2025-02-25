3f626122f9763ba2bf715033d4a64f7c867dd44d
ADNAN ALI BHUTTO <138211266+adnanbhutto786@users.noreply.github.com>
2025-02-26 00:11:48 +0500

    start  basic introduction to python

---
+++ version.py
@@ -0,0 +1,16 @@
+# import sys
+# print(sys.version)
+
+if 5>2:
+    print("Five is greater than two");
+
+if 1<3:
+      print("One is smaller than three");
+print("hello")
+
+
+x=5;
+y="Hello,Adnan";
+print(x)
+print(y)
+
