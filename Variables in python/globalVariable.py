b95c1a60df9975accda6ed3b85866fbbf7b52588
ADNAN ALI BHUTTO <138211266+adnanbhutto786@users.noreply.github.com>
2025-02-26 10:13:00 +0500

    Complete  variable topic like global and local variable

---
+++ Variables in python/globalVariable.py
@@ -0,0 +1,38 @@
+# x="awesome";
+# def myfunc():
+#     print("Python is " + x);
+# myfunc();
+#
+# y="Adnan";
+# def myName():
+#     print("My Name is " + y)
+# myName();
+#
+#
+# z="Ahsan";
+# def myAlternnateName():
+#     print("Bhutto")
+# myAlternnateName();
+
+# def myfun():
+#     x="Hello , World";
+#     print("Adnan " + x);
+# myfun()
+
+
+
+# x="awesome";
+# def myfuni():
+#     y="Beautiful";
+#     print("Flower is " + y);           local varible inside a function  and global variable outside of function....
+# myfuni();
+# print("python is " + x)
+
+
+def myfunction():
+    global x;
+    x="Fantastic";
+
+myfunction()
+print("Python is " + x)
+
